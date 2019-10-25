d08af4e89405c9fc8ca92bb548fa6458e0029730
Zacharya <zacharya19@gmail.com>
2019-10-25 22:45:50 +0300

    Diff msg when no owners (#7)

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const handlePullRequestChange = require('./lib/handle-pull-request-change')
 
 module.exports = app => {
-  // TODO: watch OWNER file changes
+  // TODO: watch OWNER file for changes
   app.on([
+    'pull_request.reopened',
     'pull_request.opened',
     'pull_request.synchronize',
     'pull_request_review.submitted'
